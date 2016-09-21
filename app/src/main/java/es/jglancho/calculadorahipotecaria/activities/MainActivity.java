3727064f545444e3d990d6baf079d305e19c609b
javierlancho <varios983@gmail.com>
2016-09-21 13:33:25 +0200

    Cambiadas cosas en la clase Main para probar el funcionamiento de Git

--- app/src/main/java/es/jglancho/calculadorahipotecaria/activities/MainActivity.java
+++ app/src/main/java/es/jglancho/calculadorahipotecaria/activities/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
 
     /**
      * onCreate()
-     * He introducido esta línea para probar La instalación de GIt
+     * He introducido esta línea para probar La instalación de GIT
      *
      */
     @Override
